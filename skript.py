8cf593294e4ae1d48efd0355f0912879c2b5bbf4
Shakal1105 <55797859+Shakal1105@users.noreply.github.com>
2023-12-07 00:29:36 +0200

    Update skript.py

--- skript.py
+++ skript.py
@@ -7,26 +7,20 @@ with open('log.txt', 'r') as file:
 
 # Створюємо списки URL та реферерів
 urls = [line.split()[6].strip('"') for line in log_lines]
-referers = [line.split()[10].strip('"') for line in log_lines if '"Referer:' in line]
-
 # Рахуємо кількість кожного URL та кількість кожного посилання від реферерів
 url_counts = Counter(urls)
-referer_counts = Counter(referers)
 
 # Знаходимо топ-10 URL
 top_urls = url_counts.most_common(10)
-
+kwarg={}
 # Виводимо результати
 print("Топ-10 URL та їх реферерів:")
+a, max_count=1, 0
 for url, count in top_urls:
-    print(f"{url} - {count} - 100%")
-    
-    # Знаходимо топ-10 реферерів для кожного URL
-    url_referers = [referer for referer in referers if url in referer]
-    referer_counts_url = Counter(url_referers)
-    
-    for referer, referer_count in referer_counts_url.most_common(10):
-        percent = (referer_count / count) * 100
-        print(f"  {referer} - {referer_count} - {percent:.2f}%")
-    
-    print()
+    kwarg[a] = {"url":url, "count":int(count)}
+    max_count=max_count+count
+    a+=1
+for i in range(1,11):
+    ar = kwarg[i]
+    counter = (ar["count"]*100)/max_count
+    print(f" {ar['url']}  - {ar['count']} - {counter:.2f}%")
